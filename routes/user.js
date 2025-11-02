7c82a32a4972b37d8b19b8fdf8d58b473fab98b1
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-11-01 20:04:29 -0600

    user files

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const utils = require('../validation/utils');
 
 router.get("/user", userControllers.getAllUsers);
 router.get("/user/:id", utils.validateId, userControllers.getUserID);
-router.post("/user", utils.validateUser, userControllers.addUser);
+router.post("/user", userControllers.addUser);
 router.put("/user/:id", utils.validateUser, utils.validateId, userControllers.updateUser);
 router.delete("/user/:id", utils.validateId, userControllers.deleteUser);
 
